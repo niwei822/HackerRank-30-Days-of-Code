84320032517f0deeaac41c6ad031e981725399ad
niwei822 <cecily.li.822@gmail.com>
2022-10-10 15:38:28 -0700

    Day1 completed

---
+++ HackerRank30DayofCode.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+//Day1
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+// Declare second integer, double, and String variables.
+// Read and save an integer, double, and String to your variables.
+// Print the sum of both integer variables on a new line.
+// Print the sum of the double variables on a new line.
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+let a = readLine()
+let b = readLine()
+let c = readLine()
+var a1 = Int(a!)
+var b1 = Double(b!)
+var c1 = String(c!)
+print(i + a1!)
+print(d + b1!)
+print(s + c1)
+
+
