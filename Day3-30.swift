a7267a90f267b5d433d6f4c2915304dba0d8c219
niwei822 <cecily.li.822@gmail.com>
2022-10-13 17:05:18 -0700

    day4 class and instance completed

--- Day3-30.swift
+++ Day3-30.swift
@@ -23,3 +23,61 @@ switch (N % 2 == 0, N >= 6 && N <= 20) {
         default:
         print("Weird")
     }
+
+//Day4
+/*Write a Person class with an instance variable,age , and a constructor that takes an integer, ,initialAge as a parameter. The constructor must assign initialAge to age after confirming the argument passed as initialAge is not negative; if a negative argument is passed as initialAge, the constructor should set age  to  0 and print Age is not valid, setting age to 0.. In addition, you must write the following instance methods:
+ 
+ yearPasses() should increase the age instance variable by 1.
+ amIOld() should perform the following conditional actions:
+ If age < 13 , print You are young..
+ If age >=13  and age < 18 , print You are a teenager..
+ Otherwise, print You are old..*/
+
+class Person {
+    var age: Int = 0
+
+    init(initialAge: Int) {
+        // Add some more code to run some checks on initialAge
+        self.age = initialAge
+        if initialAge < 0 {
+            age = 0
+            print("Age is not valid, setting age to 0.")
+        }
+    }
+
+    func amIOld() {
+        // Do some computations in here and print out the correct statement to the console
+        if age < 13 {
+            print("You are young.")
+        }
+        else if age >= 13 && age < 18 {
+            print("You are a teenager.")
+        }
+        else {
+            print("You are old.")
+        }
+    }
+
+    func yearPasses() {
+        // Increment the age of the person in here
+        age += 1
+    }
+}
+
+let t = Int(readLine()!)!
+
+for _ in 0..<t {
+    let age = Int(readLine()!)!
+    let p = Person(initialAge: age)
+
+    p.amIOld()
+
+    for i in 1...3 {
+        p.yearPasses()
+    }
+
+    p.amIOld()
+
+    print("")
+}
+
