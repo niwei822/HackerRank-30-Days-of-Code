4db7278e80387b2af6b31b95bca485b21a67950d
niwei822 <cecily.li.822@gmail.com>
2022-10-18 10:17:41 -0700

    Day9 Recursive Method for Calculating Factorial completed

--- Day3-30.swift
+++ Day3-30.swift
@@ -161,3 +161,33 @@ while let name = readLine() {
         print("Not found")
     }
 }
+
+//Day9
+//Recursive Method for Calculating Factorial
+/*
+ * Complete the 'factorial' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+func factorial(n: Int) -> Int {
+    // Write your code here
+if n <= 1 {
+    return 1
+}
+return n * factorial(n: n - 1)
+}
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+let result = factorial(n: n)
+
+fileHandle.write(String(result).data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
+
