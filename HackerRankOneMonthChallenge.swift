eb5eedcca7576485a9732443866e79d0bec4a071
niwei822 <cecily.li.822@gmail.com>
2022-10-19 14:15:38 -0700

    completed 'diagonalDifference'

--- HackerRankOneMonthChallenge.swift
+++ HackerRankOneMonthChallenge.swift
@@ -183,3 +183,51 @@ func matchingStrings1(strings: [String], queries: [String]) -> [Int] {
     }
     return count
 }
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+func diagonalDifference(arr: [[Int]]) -> Int {
+    // Write your code here
+var sum1 = 0
+var sum2 = 0
+for i in 0..<arr.count {
+    sum1 += arr[i][i]
+    sum2 += arr[i][arr.count - 1 - i]
+}
+return abs(sum1 - sum2)
+}
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+var arr = [[Int]]()
+
+for _ in 1...n {
+    guard let arrRowTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+    let arrRow: [Int] = arrRowTemp.split(separator: " ").map {
+        if let arrItem = Int($0) {
+            return arrItem
+        } else { fatalError("Bad input") }
+    }
+
+    guard arrRow.count == n else { fatalError("Bad input") }
+
+    arr.append(arrRow)
+}
+
+guard arr.count == n else { fatalError("Bad input") }
+
+let result = diagonalDifference(arr: arr)
+
+fileHandle.write(String(result).data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
+
