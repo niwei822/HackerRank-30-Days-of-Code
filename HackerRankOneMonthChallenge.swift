14a6a103254e3c5828781f05dcf7e7fa9d611b40
niwei822 <cecily.li.822@gmail.com>
2022-10-18 20:33:00 -0700

    complete 'timeConversion'

--- HackerRankOneMonthChallenge.swift
+++ HackerRankOneMonthChallenge.swift
@@ -70,3 +70,39 @@ guard arr.count == 5 else { fatalError("Bad input") }
 
 miniMaxSum(arr: arr)
 
+/*Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+ 
+ Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+ - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.*/
+/*
+ * Complete the 'timeConversion' function below.
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+func timeConversion(s: String) -> String {
+    // Write your code here
+let arr = s.components(separatedBy: ":")
+let i = arr.count - 1
+let time = arr[i].dropLast(2)
+let new = s.dropLast(2)
+if arr[i].contains("P") && arr[0] != "12" {
+    let hour = Int(arr[0])! + 12
+     return "\(hour):\(arr[1]):\(time)"
+} else if arr[i].contains("A") && arr[0] == "12" {
+    return "00:\(arr[1]):\(time)"
+} else {
+    return String(new)
+}
+}
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let s = readLine() else { fatalError("Bad input") }
+
+let result = timeConversion(s: s)
+
+fileHandle.write(result.data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
+
