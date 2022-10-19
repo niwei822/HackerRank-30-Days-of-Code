4f05c53a89d8f7d4f47147972ef1e94144fa5083
niwei822 <cecily.li.822@gmail.com>
2022-10-18 19:31:05 -0700

    complete miniMaxSum

--- HackerRankOneMonthChallenge.swift
+++ HackerRankOneMonthChallenge.swift
@@ -43,3 +43,30 @@ guard arr.count == n else { fatalError("Bad input") }
 
 plusMinus(arr: arr)
 
+/*Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+ * Complete the 'miniMaxSum' function below.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func miniMaxSum(arr: [Int]) -> Void {
+    // Write your code here
+let newArr = arr.sorted()
+var sum = 0
+for i in 0..<newArr.count {
+    sum += newArr[i]
+}
+print("\(sum - newArr[newArr.count - 1]) \(sum - newArr[0])")
+}
+
+guard let arrTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let arr: [Int] = arrTemp.split(separator: " ").map {
+    if let arrItem = Int($0) {
+        return arrItem
+    } else { fatalError("Bad input") }
+}
+
+guard arr.count == 5 else { fatalError("Bad input") }
+
+miniMaxSum(arr: arr)
+
