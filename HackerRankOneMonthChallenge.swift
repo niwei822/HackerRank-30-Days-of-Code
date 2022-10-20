3d539668045539ab841ffe98ebd42b4149a20522
niwei822 <cecily.li.822@gmail.com>
2022-10-19 19:38:45 -0700

    complete 'countingSort'

--- HackerRankOneMonthChallenge.swift
+++ HackerRankOneMonthChallenge.swift
@@ -231,3 +231,39 @@ let result = diagonalDifference(arr: arr)
 fileHandle.write(String(result).data(using: .utf8)!)
 fileHandle.write("\n".data(using: .utf8)!)
 
+/*
+ * Complete the 'countingSort' function below.
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func countingSort(arr: [Int]) -> [Int] {
+    // Write your code here
+    var result = [Int](repeating: 0, count: 100)
+    for i in 0...arr.count - 1 {
+        result[arr[i]] += 1
+    }
+    return result
+}
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let arrTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let arr: [Int] = arrTemp.split(separator: " ").map {
+    if let arrItem = Int($0) {
+        return arrItem
+    } else { fatalError("Bad input") }
+}
+
+guard arr.count == n else { fatalError("Bad input") }
+
+let result = countingSort(arr: arr)
+
+fileHandle.write(result.map{ String($0) }.joined(separator: " ").data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
