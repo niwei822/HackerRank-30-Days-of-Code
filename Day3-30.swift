6687abdb8150763f282e551fe45182e3fa850c19
niwei822 <cecily.li.822@gmail.com>
2022-10-30 13:15:59 -0700

    Day21 Generics completed

--- Day3-30.swift
+++ Day3-30.swift
@@ -478,7 +478,7 @@ class Solution {
 // Write your code here
 var numSwaps = 0
 for i in 0..<n {
-    for j in 0..<n - 1 {
+    for j in 0..<n - i - 1 {
         if a[j] > a[j+1] {
             let temp = a[j]
             a[j] = a[j+1]
@@ -490,3 +490,13 @@ for i in 0..<n {
 print("Array is sorted in \(numSwaps) swaps.")
 print("First Element: \(a[0])")
 print("Last Element: \(a[n - 1])")
+
+//Day 21: Generics
+// Write your code here
+    func printArray(array : [T]) {
+        for i in array {
+            print(i)
+        }
+    }
+}
+
