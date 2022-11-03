f893bd6a33fbf0a4366078fea76cac8cb1c85597
niwei822 <cecily.li.822@gmail.com>
2022-11-03 15:07:14 -0700

    Day25 checkPrime completed

--- Day3-30.swift
+++ Day3-30.swift
@@ -604,3 +604,20 @@ class Tree {
         }
         return head
     }
+//Day25 checkPrime
+    func checkPrime(n: Int) -> String {
+        if n == 0 || n == 1 {
+            return "Prime"
+        }
+        var count = 0
+        for i in 1...n {
+            if n % i == 0 {
+                count += 1
+            }
+        }
+        if count > 2 {
+            return "Not Prime"
+        }
+        return "Prime"
+    }
+    checkPrime(n: 71)
